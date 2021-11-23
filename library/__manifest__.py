3fdbcf6b596187cd776cb9903240abb723d896b7
minaxivanani <94071972+minaxivanani@users.noreply.github.com>
2021-11-24 00:35:44 +0530

    Add files via upload

---
+++ library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+  'name':'Gvp Library',
+  'category':'Sales',
+  'application':True,
+
+  'data':[
+
+           'security/ir.model.access.csv',
+           'views/library_menu.xml',
+           
+    ],
+
+ 
+
+
+}
